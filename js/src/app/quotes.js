c2fccdf000ffdf09ec34b19be80d865285ade723
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2017-04-27 01:38:22 +0100

    Custom typing and deleting

--- js/src/app/quotes.js
+++ js/src/app/quotes.js
@@ -4,13 +4,14 @@ var Quotes = {
     init: function() {
         this.getNew()
             .then((quotes) => {
-            	this.loaded = quotes;
-            	this.currentPage += 1;
-            	console.log('loaded Quotes')
+                this.loaded = quotes;
+                this.currentPage += 1;
+                console.log('loaded Quotes')
             })
             .then(() => {
-            	console.log('Start typing')
-            	this.startTyping()
+                console.log('Start typing')
+                    // this.startTyping()
+                this.type();
             })
     },
     getNew: function() {
@@ -19,19 +20,44 @@ var Quotes = {
             .then((response) => response.data);
     },
     startTyping: function() {
-    	let strings = _.map(this.loaded, 'quote');
-		$('#quote').typeIt({
-		    strings: strings,
-		    speed: 150,
-     		autoStart: false,
-     		lifeLike: true,
-     		breakLines: false,
-     		startDelete: true,
-     		breakDelay: 3000
-		});
-    }
+        let strings = _.map(this.loaded, 'quote');
+        $('#quote').typeIt({
+            strings: strings,
+            speed: 150,
+            autoStart: false,
+            lifeLike: true,
+            breakLines: false,
+            startDelete: true,
+            breakDelay: 3000
+        });
+    },
+    type: function() {
+        let strings = _.map(this.loaded, 'quote');
+        return Promise.resolve(strings)
+            .then(function typeItem(strings) {
+                    if (!strings) return;
+
+                    var timeouts = [];
+                    var text = strings.shift();
+                    var quote = document.getElementById('quote');
+
+                    var delay_time = 0;
+                    text.split('').forEach(function(letter, i) {
+                        delay_time += 75;
+                        timeouts.push(setTimeout(function() {
+                            quote.innerHTML = quote.innerHTML + letter;
+                            if ((i + 1) == text.length) {
+                            	setTimeout(function() {
+                            		quote.innerHTML = "";
+                            		return Promise.resolve(strings).then(typeItem);
+                            	}, 2000);
+                            }
+                        }, delay_time));
+                    });
+                })
+	}
 }
 
 $(function() {
-	Quotes.init();
+    Quotes.init();
 })
