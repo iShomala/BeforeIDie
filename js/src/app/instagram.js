87a185aa9213531ec5dc5806340508fbc283c010
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2017-07-04 22:28:01 +0100

    Changes to gallery section

--- js/src/app/instagram.js
+++ js/src/app/instagram.js
@@ -5,7 +5,7 @@ var feed = new Instafeed({
     clientId: 'e69dc5d429644873976199b5ee9ff8f5',
     accessToken: '590563274.e69dc5d.d24b4284737a4a68ab70183ceaa77de9',
     target: 'gallery',
-    limit: 8,
+    limit: 6,
     sortBy: 'most-liked',
     resolution: 'standard_resolution'
 });
