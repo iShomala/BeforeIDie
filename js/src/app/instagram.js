15674ec04a72aecd282763cb28e8912d8baf82b7
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2017-07-04 01:58:41 +0100

    Add Instagram feed

---
+++ js/src/app/instagram.js
@@ -0,0 +1,12 @@
+var feed = new Instafeed({
+    get: 'user',
+    // userId: 4336590343,
+    userId: 590563274,
+    clientId: 'e69dc5d429644873976199b5ee9ff8f5',
+    accessToken: '590563274.e69dc5d.d24b4284737a4a68ab70183ceaa77de9',
+    target: 'gallery',
+    limit: 8,
+    sortBy: 'most-liked',
+    resolution: 'standard_resolution'
+});
+feed.run();
