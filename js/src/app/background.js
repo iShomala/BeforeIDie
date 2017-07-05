181500560a496ca5cdfd8889462e3a38a03a835d
Sandra Wellington <swellingtion@outlook.com>
2017-07-05 15:18:23 +0100

    changed colors

--- js/src/app/background.js
+++ js/src/app/background.js
@@ -1,9 +1,8 @@
 // need to curate better colors tho
-let colors = ['#F42C04', '#0CCE6B', '#182825', '#016FB9', '#770058', '#D0021B'];
+let colors = ['#E51B26', '#FFF219', '#C0D537', '#B71A8C', '#19378F', '#5F6061'];
 
 (function myLoop (i) {          
    setTimeout(function () {   
-      console.log(i);
       $('#quotes').css('background-color', colors[i % colors.length])               
       myLoop(++i);      
    }, 45000)
